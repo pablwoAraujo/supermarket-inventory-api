69968fbd8441534a76fb23761d9647751c344c9b
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-04-11 10:05:54 -0300

    feat: adicionando o middleware de autenticação

--- api/routes/usuarioRoute.js
+++ api/routes/usuarioRoute.js
@@ -1,13 +1,16 @@
-const { Router } = require('express')
-const UsuarioController = require('../controllers/usuarioController')
+const { Router } = require('express');
+const UsuarioController = require('../controllers/usuarioController');
+const autenticado = require('../middleware/autenticado');
 
-const router = Router()
+const router = Router();
+
+router.use(autenticado);
 
 router
   .post('/usuario', UsuarioController.cadastrarUsuario)
   .get('/usuario', UsuarioController.buscarTodosUsuarios)
   .get('/usuario/id/:id', UsuarioController.buscarUsuarioPorId)
   .delete('/usuario/id/:id', UsuarioController.deletarUsuarioPorId)
-  .put('/usuario/id/:id', UsuarioController.editarUsuario)
+  .put('/usuario/id/:id', UsuarioController.editarUsuario);
 
-module.exports = router
+module.exports = router;
