e1b082f3ea30bafa83e2486c46e9dd8530f1be92
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-04-19 17:51:32 -0300

    feat: adicionando um middleware para verificar as permissoes

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -3,12 +3,13 @@ const ProdutoController = require('../controllers/produtoController');
 
 const router = Router();
 const roles = require('../middleware/roles');
+const permissoes = require('../middleware/permissooes');
 
 router
-  .post('/produto', roles(['adm']), ProdutoController.cadastrarProduto)
-  .get('/produto', roles(['usuario', 'gerente', 'adm']), ProdutoController.buscarTodosProdutos)
-  .get('/produto/id/:id', roles(['usuario', 'gerente', 'adm']), ProdutoController.buscarProdutoPorId)
-  .delete('/produto/id/:id',roles(['adm']), ProdutoController.deletarProdutoPorId)
-  .put('/produto/id/:id', roles(['gerente', 'adm']), ProdutoController.editarProduto);
+  .post('/produto', roles(['adm']), permissoes(['criar produto']), ProdutoController.cadastrarProduto)
+  .get('/produto', roles(['usuario', 'gerente', 'adm']), permissoes(['ver produto']), ProdutoController.buscarTodosProdutos)
+  .get('/produto/id/:id', roles(['usuario', 'gerente', 'adm']), permissoes(['ver produto']), ProdutoController.buscarProdutoPorId)
+  .delete('/produto/id/:id',roles(['adm']), permissoes(['remover produto']), ProdutoController.deletarProdutoPorId)
+  .put('/produto/id/:id', roles(['gerente', 'adm']), permissoes(['editar produto']), ProdutoController.editarProduto);
 
 module.exports = router;
