be5bc22689885f55d9b4f093036e12ffdfc4f8de
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-04-19 17:37:21 -0300

    feat: criando um middleware para verificar as roles

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,13 +1,14 @@
-const { Router } = require('express')
-const ProdutoController = require('../controllers/produtoController')
+const { Router } = require('express');
+const ProdutoController = require('../controllers/produtoController');
 
-const router = Router()
+const router = Router();
+const roles = require('../middleware/roles');
 
 router
-  .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', ProdutoController.buscarTodosProdutos)
-  .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
-  .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
-  .put('/produto/id/:id', ProdutoController.editarProduto)
+  .post('/produto', roles(['adm']), ProdutoController.cadastrarProduto)
+  .get('/produto', roles(['usuario', 'gerente', 'adm']), ProdutoController.buscarTodosProdutos)
+  .get('/produto/id/:id', roles(['usuario', 'gerente', 'adm']), ProdutoController.buscarProdutoPorId)
+  .delete('/produto/id/:id',roles(['adm']), ProdutoController.deletarProdutoPorId)
+  .put('/produto/id/:id', roles(['gerente', 'adm']), ProdutoController.editarProduto);
 
-module.exports = router
+module.exports = router;
